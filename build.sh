35b504b35bd3580998ed421aa15f9ef665b20325
Shuo Chen <chenshuo@chenshuo.com>
2016-12-28 00:12:51 -0800

    try clang on travis

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -x
 SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-../build}
 BUILD_TYPE=${BUILD_TYPE:-release}
+CXX=${CXX:-g++}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
 BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
 
@@ -12,6 +13,7 @@ mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
   && cd $BUILD_DIR/$BUILD_TYPE-cpp11 \
   && cmake \
            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+           -DCMAKE_CXX_COMPILER=$CXX \
            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
            -DCMAKE_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
            $SOURCE_DIR \
